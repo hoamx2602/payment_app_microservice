2f65a372fe32e453280ec323f7abf5bbf6ec44ee
hoamx <hoa.mx@kiotviet.com>
2024-04-24 15:50:35 +0700

    build: setup log with pino

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -2,15 +2,33 @@ import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
-import { ReservationDocument, ReservationRepository, ReservationSchema } from '@app/common/schemas';
+import {
+  ReservationDocument,
+  ReservationRepository,
+  ReservationSchema,
+} from '@app/common/schemas';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([
-    {
-      name: ReservationDocument.name,
-      schema: ReservationSchema
-    }
-  ])],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      {
+        name: ReservationDocument.name,
+        schema: ReservationSchema,
+      },
+    ]),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true
+          }
+        }
+      }
+    })
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationRepository],
 })
