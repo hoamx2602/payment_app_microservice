b708f822987f3881c54a1292b81d6a18bf0cc421
hoamx <hoa.mx@kiotviet.com>
2024-05-02 14:34:49 +0700

    fix: current user decorator

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,8 +1,7 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
-import { CurrentUser } from '../decorators';
-import { UserDocument } from '@app/common';
+import { CurrentUser, UserDocument } from '@app/common';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Controller('users')
