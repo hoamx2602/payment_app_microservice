db0b8c163f13edf471f2a59ea0504389ad4fac30
hoamx <hoamx.work@gmail.com>
2024-05-02 21:49:19 +0700

    feat: handle create simple payment

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,7 @@
+import { CreateChargeDto } from '@app/common';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
-import { CreateChargeDto } from './dto';
 
 @Injectable()
 export class PaymentsService {
@@ -11,18 +11,16 @@ export class PaymentsService {
 
   constructor(private readonly configService: ConfigService) {}
 
-  async createCharge ({ card, amount }: CreateChargeDto) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
-    });
-
+  async createCharge ({ amount }: CreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       payment_method: 'pm_card_visa',
       amount: amount * 100,
       confirm: true,
-      payment_method_types: ['card'],
       currency: 'usd',
+      automatic_payment_methods: {
+        allow_redirects: 'never',
+        enabled: true
+      }
     });
     
     return paymentIntent;
--- apps/reservations/src/reservations.service.ts
+++ apps/reservations/src/reservations.service.ts
@@ -4,23 +4,26 @@ import { UpdateReservationDto } from './dto/update-reservation.dto';
 import { ReservationRepository } from '@app/common/schemas';
 import { CREATE_CHARGE_MSG, PAYMENTS_SERVICE } from '@app/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { map } from 'rxjs';
 
 @Injectable()
 export class ReservationsService {
   constructor(
     private readonly reservationRepository: ReservationRepository,
-    @Inject(PAYMENTS_SERVICE) private readonly paymentsService: ClientProxy
+    @Inject(PAYMENTS_SERVICE) private readonly paymentsService: ClientProxy,
   ) {}
   async create(createReservationDto: CreateReservationDto, userId: string) {
-    this.paymentsService.send(CREATE_CHARGE_MSG, createReservationDto.charge).subscribe(async (response) => {
-      console.log(response);
-      const reservation = await this.reservationRepository.create({
-        ...createReservationDto,
-        timestamp: new Date(),
-        user_id: userId
-      });
-    });
-
+    return this.paymentsService
+      .send(CREATE_CHARGE_MSG, createReservationDto.charge)
+      .pipe(
+        map(() => {
+          return this.reservationRepository.create({
+            ...createReservationDto,
+            timestamp: new Date(),
+            user_id: userId,
+          });
+        }),
+      );
   }
 
   async findAll() {
@@ -28,17 +31,17 @@ export class ReservationsService {
   }
 
   async findOne(_id: string) {
-    return this.reservationRepository.findOne({_id});
+    return this.reservationRepository.findOne({ _id });
   }
 
   async update(_id: string, updateReservationDto: UpdateReservationDto) {
     return this.reservationRepository.findOneAndUpdate(
       { _id },
-      { $set: updateReservationDto }
+      { $set: updateReservationDto },
     );
   }
 
   async remove(_id: string) {
-    return this.reservationRepository.findOneAndDelete({ _id })
+    return this.reservationRepository.findOneAndDelete({ _id });
   }
 }
