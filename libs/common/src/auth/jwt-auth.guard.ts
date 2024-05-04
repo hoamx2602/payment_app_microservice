9bfe5279297311d650a95645cfeb4359fc80daa0
hoamx <hoamx.work@gmail.com>
2024-05-05 01:20:54 +0700

    feat: add grpc ts files and refactor jwt-auth.guard using gRPC

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -1,23 +1,37 @@
-import { CanActivate, ExecutionContext, ForbiddenException, Inject, Injectable, Logger } from "@nestjs/common";
-import { Observable, catchError, map, of, tap } from "rxjs";
-import { AUTHENTICATE_MSG, AUTH_SERVICE } from "../constants";
-import { ClientProxy } from "@nestjs/microservices";
-import { UserDto } from "../dto";
-import { Reflector } from "@nestjs/core";
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+import { AUTH_SERVICE_NAME, AuthServiceClient } from '../types';
 
 @Injectable()
-export class JwtAuthGuard implements CanActivate {
+export class JwtAuthGuard implements CanActivate, OnModuleInit {
   private readonly logger = new Logger(JwtAuthGuard.name);
+  private authService: AuthServiceClient;
   constructor(
-    @Inject(AUTH_SERVICE) private readonly authClient: ClientProxy,
-    private readonly reflector: Reflector
+    @Inject(AUTH_SERVICE_NAME) private readonly client: ClientGrpc,
+    private readonly reflector: Reflector,
   ) {}
+
+  onModuleInit() {
+    this.authService =
+      this.client.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+  }
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    const jwt = request.cookies?.Authentication || request?.headers.authentication;
+    const jwt =
+      request.cookies?.Authentication || request?.headers.authentication;
 
     if (!jwt) {
       return false;
@@ -25,25 +39,30 @@ export class JwtAuthGuard implements CanActivate {
 
     const roles = this.reflector.get<string[]>('roles', context.getHandler());
 
-    return this.authClient.send<UserDto>(AUTHENTICATE_MSG, {
-      Authentication: jwt
-    }).pipe(
-      tap((res) => {
-        if (roles) {
-          for(const role of roles) {
-            if (!res.roles?.includes(role)) {
-              this.logger.error("The user does not have valid role")
-              throw new ForbiddenException();
+    return this.authService
+      .authenticate({
+        Authentication: jwt,
+      })
+      .pipe(
+        tap((res) => {
+          if (roles) {
+            for (const role of roles) {
+              if (!res.roles?.includes(role)) {
+                this.logger.error('The user does not have valid role');
+                throw new ForbiddenException();
+              }
             }
           }
-        }
-        context.switchToHttp().getRequest().user = res;
-      }),
-      map(() => true),
-      catchError((err) => {
-        this.logger.error(err);
-        return of(false)
-      })
-    )
+          context.switchToHttp().getRequest().user = {
+            ...res,
+            _id: res.id
+          };
+        }),
+        map(() => true),
+        catchError((err) => {
+          this.logger.error(err);
+          return of(false);
+        }),
+      );
   }
 }
