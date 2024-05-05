d98cc5380d806254b0da9721fc8a093b7f994e1a
hoamx <hoamx.work@gmail.com>
2024-05-05 19:52:14 +0700

    feat: setup prisma client for reservation

---
+++ apps/reservations/prisma/migrations/20240505124819_reservation/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Reservation" (
+    "id" SERIAL NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "start_data" TIMESTAMP(3) NOT NULL,
+    "end_date" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "invoice_id" TEXT NOT NULL,
+
+    CONSTRAINT "Reservation_pkey" PRIMARY KEY ("id")
+);
